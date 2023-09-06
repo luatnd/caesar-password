5efc58e21dc27ba8cf2e793b05381f423dda44fb
neo.mxn0 <neo.mxn0@gmail.com>
2023-09-07 02:01:31 +0700

    Upgrade next to 13.4 and rm next export from CD workflow

--- next.config.js
+++ next.config.js
@@ -13,6 +13,16 @@ const nextConfig = {
   //     '/': { page: '/seller/0' }
   //   }
   // },
+
+  // - error "next export" does not work with App Router.
+  // Please use "output: export" in next.config.js
+  // https://nextjs.org/docs/advanced-features/static-html-export
+  //
+  // TLDR;
+  // After running next build, Next.js will produce an `out` folder
+  // which contains the HTML/CSS/JS assets for your application.
+  // so we don't need `next export` anymore if we use this option
+  output: "export",
 }
 
 module.exports = nextConfig
